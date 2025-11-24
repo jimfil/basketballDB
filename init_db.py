b1251f263b309162bae030a6456c9942206f9a58
jimfil <119121972+jimfil@users.noreply.github.com>
2025-11-24 11:18:06 +0200

    removed creds

--- init_db.py
+++ init_db.py
@@ -1,15 +1,17 @@
 import pymysql
 import certifi
+import os
+from dotenv import load_dotenv
 
-# CONFIGURATION (Must match your settings.py)
-DB_HOST = 'gateway01.eu-central-1.prod.aws.tidbcloud.com'      
-DB_USER = '2HAKkCduYQbS5n9.root'  
-DB_PASS = 'b9Iq1COyU4KdwnTc'   
-DB_NAME = 'match_db' 
+# 1. Load the variables from the .env file
+load_dotenv()
 
-
-print("1. Connecting to TiDB Server...")
-# Connect to the server generally (no specific DB yet)
+# 2. Fetch variables (Best practice: use all caps for constants)
+# If the variable isn't found, these will be None
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_NAME   = os.getenv('DB_NAME')
 conn = pymysql.connect(
     host=DB_HOST, 
     user=DB_USER, 

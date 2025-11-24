c0601175735c24e8911b6616a3a7c69e3eca6acd
Andreas <a.panagopsch@gmail.com>
2025-11-24 11:28:27 +0200

    ok

--- model.py
+++ model.py
@@ -12,17 +12,19 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_NAME   = os.getenv('DB_NAME')
-conn = pymysql.connect(
-    host=DB_HOST, 
-    user=DB_USER, 
-    password=DB_PASS, 
-    port=4000,
-    ssl={'ca': certifi.where()}
-)
 
-cur = conn.cursor()
+def getConnection():
+    conn = pymysql.connect(
+        host=DB_HOST, 
+        user=DB_USER, 
+        password=DB_PASS, 
+        port=4000,
+        ssl={'ca': certifi.where()}
+    )
+    return conn
+
+cur = getConnection.cursor()
 cur.execute(f"use {DB_NAME};SELECT * FROM Team;")
 for r in cur:
     print(r)
 cur.close()
-conn.close()

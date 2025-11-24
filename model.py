e526850b35dbdedbb4ae146c50c1dc3e95cd9d01
jimfil <119121972+jimfil@users.noreply.github.com>
2025-11-24 14:48:36 +0200

    read_table_entries

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_NAME   = os.getenv('DB_NAME')
 
-def getConnection():
+def get_connection():
     con = pymysql.connect(
         host=DB_HOST, 
         user=DB_USER, 
@@ -26,7 +26,7 @@ def getConnection():
 
 def query(sql, params=()):
     returnable = []
-    with getConnection() as con:
+    with get_connection() as con:
         with con.cursor() as cur:
             cur.execute(sql, params)
             cols = [d[0] for d in cur.description]
@@ -35,17 +35,43 @@ def query(sql, params=()):
             return returnable               #epistrefei array pou se kathe thesi exei ena leksiko me key to id 
     
 
-def getTeams():
+def get_teams():
     return query("SELECT * FROM Team;")
 
-def getPlayers():
+def get_players():
     return query("SELECT * FROM Person;")
 
-def createTeam(name):
-    with getConnection() as con:
+def create_team(name):
+    with get_connection() as con:
         cur = con.cursor()
         try:
             cur.execute("INSERT INTO Team (NAME) VALUES (%s);",  (name))
             con.commit()
         except pymysql.err.IntegrityError as e:
             return  # IF e==pymysql.err.IntegrityError yparxei diplo onoma
+
+def return_cud_tables():
+    with get_connection() as con:
+        with con.cursor() as cur:
+            cur.execute("SHOW TABLES;")
+            tables = cur.fetchall()
+            return [tables[0] for tables in tables]
+        
+def return_attributes(table_name):
+    with get_connection() as con:
+        with con.cursor() as cur:
+            cur.execute(f"SHOW COLUMNS FROM {table_name};")
+            columns = cur.fetchall()
+            return [col[0] for col in columns]
+        
+def read_table_entries_for_attribute(table_name,list_table_attribute):
+    with get_connection() as con:
+        with con.cursor() as cur:
+            lista =[]
+            for table_attribute in list_table_attribute:
+                cur.execute(f"SELECT {table_attribute} FROM {table_name};")
+                lista.append([entry[0] for entry in cur])
+            return lista
+            
+        
+print(read_table_entries_for_attribute("Team","name"))

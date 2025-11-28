0db7459b32f14bddf79795efced05ef3eb964d63
jimfil <119121972+jimfil@users.noreply.github.com>
2025-11-28 11:38:45 +0200

    playerStats

--- controller.py
+++ controller.py
@@ -3,13 +3,15 @@ from view import *
 
 def find_playerstats(player_id):
     for i in range(1000):
-        print(f"\n\tPage {i + 1}\t")
-        results = get_player_stats(player_id, i)
+        print(f"\nPage {i + 1}\t")
+        print(f"{'Game':<8}{'Event Made':<28}{'Time'}") #dwse toulaxiston 28 spaces apo event mexri to time klp
+        results = get_player_stats(player_id, i)    #epistrefei Game -> Event Name -> Time
         if not results:
             print("No more stats found for this Player.")
             break
         for row in results:
-            print(row)
+            match_id, event_name, game_time = row   
+            print(f"{match_id:<8}{event_name:<28}{str(game_time)}")
         user_input = input("\nPress [Enter] to get next 10, or type 'q' to break: ")
         if user_input.lower() == 'q':
             print("Exiting...")
@@ -29,5 +31,6 @@ def find_matchstats(match_id):
             print("Exiting...")
             break
 
-if __name__=="main":
-    kati()
+if __name__ == "__main__":
+
+    find_playerstats(10000)

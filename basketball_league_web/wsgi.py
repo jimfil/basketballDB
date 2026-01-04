44adf712097c756a5b1080e744dd4acd7f70dd97
Andreas <a.panagopsch@gmail.com>
2026-01-04 19:24:00 +0200

    fggrg hate this

--- basketball_league_web/wsgi.py
+++ basketball_league_web/wsgi.py
@@ -1,15 +1,4 @@
-import sys
-import os
-
-
-project_home = '/home/andrupe/basketballDB'
-app_dir = os.path.join(project_home, 'basketball_league_web')
-
-if app_dir not in sys.path:
-    sys.path.insert(0, app_dir)
-
 from app import create_app
-from config import Config
+from config import DevelopmentConfig # Or ProductionConfig if you have it
 
-# Create the Flask app instance for production
-application = create_app(Config)
+app = create_app(DevelopmentConfig)
